6c49e6d4b4ad09d8380a71660ef1f2183607b5c7
4p73m <62073156+4p73m@users.noreply.github.com>
2021-03-28 22:53:08 +0300

    Add files via upload

---
+++ SmartRoads/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace SmartRoads
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.labelSpeed = new System.Windows.Forms.Label();
+            this.labelSpeedLimit = new System.Windows.Forms.Label();
+            this.textBoxSpeedLimit = new System.Windows.Forms.TextBox();
+            this.textBoxSensorNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(238, 111);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(69, 28);
+            this.buttonRefresh.TabIndex = 0;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // labelSpeed
+            // 
+            this.labelSpeed.AutoSize = true;
+            this.labelSpeed.Location = new System.Drawing.Point(19, 119);
+            this.labelSpeed.Name = "labelSpeed";
+            this.labelSpeed.Size = new System.Drawing.Size(110, 13);
+            this.labelSpeed.TabIndex = 1;
+            this.labelSpeed.Text = "RecommendedSpeed";
+            // 
+            // labelSpeedLimit
+            // 
+            this.labelSpeedLimit.AutoSize = true;
+            this.labelSpeedLimit.Location = new System.Drawing.Point(19, 35);
+            this.labelSpeedLimit.Name = "labelSpeedLimit";
+            this.labelSpeedLimit.Size = new System.Drawing.Size(118, 13);
+            this.labelSpeedLimit.TabIndex = 2;
+            this.labelSpeedLimit.Text = "Speed limit on the road ";
+            // 
+            // textBoxSpeedLimit
+            // 
+            this.textBoxSpeedLimit.Location = new System.Drawing.Point(149, 32);
+            this.textBoxSpeedLimit.Name = "textBoxSpeedLimit";
+            this.textBoxSpeedLimit.Size = new System.Drawing.Size(141, 20);
+            this.textBoxSpeedLimit.TabIndex = 3;
+            this.textBoxSpeedLimit.Text = "60";
+            // 
+            // textBoxSensorNumber
+            // 
+            this.textBoxSensorNumber.Location = new System.Drawing.Point(149, 6);
+            this.textBoxSensorNumber.Name = "textBoxSensorNumber";
+            this.textBoxSensorNumber.Size = new System.Drawing.Size(141, 20);
+            this.textBoxSensorNumber.TabIndex = 5;
+            this.textBoxSensorNumber.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Sensor number ";
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.Location = new System.Drawing.Point(22, 75);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.Size = new System.Drawing.Size(268, 20);
+            this.textBoxFile.TabIndex = 7;
+            this.textBoxFile.Text = "C:\\Users\\Admin\\Desktop\\smart_road_measurements.csv";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "The path to the file";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 151);
+            this.Controls.Add(this.textBoxFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxSensorNumber);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSpeedLimit);
+            this.Controls.Add(this.labelSpeedLimit);
+            this.Controls.Add(this.labelSpeed);
+            this.Controls.Add(this.buttonRefresh);
+            this.Name = "MainWindow";
+            this.Text = "MainWindow";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Label labelSpeed;
+        private System.Windows.Forms.Label labelSpeedLimit;
+        private System.Windows.Forms.TextBox textBoxSpeedLimit;
+        private System.Windows.Forms.TextBox textBoxSensorNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxFile;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SmartRoads/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SmartRoads
+{
+    public partial class MainWindow : Form
+    {
+        public static List<string> listDate = new List<string>();//Дата
+        public static List<string> listTime = new List<string>();//Время
+        public static List<string> listFriction = new List<string>();//Трение
+        public static List<string> listState = new List<string>();//Состояние
+        public static List<string> listTa = new List<string>();//Температруа воздуха
+        public static List<string> listWater = new List<string>();//Толщина слоя воды
+        public static int number = 1;
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+        }
+        /// <summary>
+        /// Заносит данные в листы
+        /// </summary>
+        /// <param name="numberOperation">количество проделанных операций</param>
+        public void DataEntry()
+        {
+            number = Convert.ToInt32(textBoxSensorNumber.Text);
+            using (var reader = new StreamReader(@textBoxFile.Text))//data path
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    //заполнение данными
+                    if (values[0].Split('.')[0] == "2021")//запись показаний датчика без дополнительной информации
+                    {
+                        listDate.Add(values[0]);
+                        listTime.Add(values[1]);
+                        listFriction.Add(values[5]);
+                        listState.Add(values[6]);
+                        listTa.Add(values[7]);
+                        listWater.Add(values[13]);
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Считает рекомендуемую скорость и изменяет отображение
+        /// </summary>
+        /// <param name="numberArea">номер строки в данных</param>
+        public void RecommendedSpeed(int numberArea)
+        {
+            //критерии снижения скорости
+            double state = Convert.ToDouble(listState[numberArea]);// состояние дороги
+            double friction = (0.81 - Convert.ToDouble(listFriction[numberArea].Replace('.', ','))) * 0.05;//трение
+            double icePossible = 0.01;//возможность гололёда
+            double water = Convert.ToDouble(listWater[numberArea].Replace('.', ',')) * 0.05;//толщина слоя воды
+
+            //изменяем стсотяние дороги
+            switch (Convert.ToInt32(state))
+            {
+                case 1:
+                    state = 0.05;
+                    break;
+                case 2:
+                    state = 0.1;
+                    break;
+                case 3:
+                    state = 0.15;
+                    break;
+                case 4:
+                    state = 0.3;
+                    break;
+                case 5:
+                    state = 0.25;
+                    break;
+                case 6:
+                    state = 0.2;
+                    break;
+            }
+            //учитываем возможное появления гололёда на участке
+            if (Convert.ToDouble(listTa[numberArea].Replace('.', ',')) < 0 & Convert.ToInt32(state) != 6)
+            {
+                icePossible = 0.15;
+            }
+            //преобразуем скорость до меньшего крастного пяти
+            int speed = Convert.ToInt32(Convert.ToInt32(textBoxSpeedLimit.Text) / (1 + state + friction + icePossible + water));
+            if (speed % 5 != 0)
+            {
+                speed -= Convert.ToInt32(speed % 5);
+            }
+            //возвращаем рекомендумую скорость
+            labelSpeed.Text = Convert.ToString(speed);
+            Thread.Sleep(2000);
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            listDate.Clear();
+            listFriction.Clear();
+            listState.Clear();
+            listTa.Clear();
+            listTime.Clear();
+            listWater.Clear();
+            DataEntry();
+            RecommendedSpeed(number);
+        }
+    }
+}
